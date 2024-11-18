1b6780c6a70ce82288892ff851665d7437f7e959
Mathias <mathias.perez@immune.institute>
2024-11-18 18:17:55 +0100

    New website

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        hero: "url('/hero-background.webp')",
+      },
+      colors: {
+        blue: "#10253E",
+        red: "#780D29",
+      },
+      container: {
+        center: true,
+        padding: "20px",
+      },
+    },
+  },
+  plugins: [],
+};
